f084738f737c9be054aafcc131ebafc2f47824de
Freemano <freemano@Freeman.local>
2024-04-02 15:20:06 +0300

    head and footer refactor

--- src/Composables/user.js
+++ src/Composables/user.js
@@ -4,13 +4,15 @@ import {auth} from "@/Composables/auth.js";
 const {authHeader,authUser,base_url,storage} =auth()
 export function user(){
     const details=ref(null)
+    const userd=ref('')
     const   userDetails = async () => {
             const res= await axios.get(base_url.value+'user', authHeader)
         if(res.status === 200){
             details.value=res.data
+            userd.value=res.data.email
         }
     }
     return{
-        authHeader,authUser,base_url,storage,userDetails,details
+        authHeader,authUser,base_url,storage,userDetails,details,userd
     }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,96 @@
 <script setup>
-import Header from "@/views/Header.vue";
+import {onMounted, ref} from "vue";
+const showProfile = ref(false)
+import {useRouter} from "vue-router";
+import {user} from "@/Composables/user.js"
 import Footer from "@/views/Footer.vue";
+const {userDetails,details} =user()
+const router = useRouter()
+const picture= ref('')
+const username= ref(null)
+username.value=details.firstName
+picture.value=details.picture
+function LogOut(){
+  localStorage.removeItem('token')
+  router.push('/auth/login/');
+}
+
+onMounted(()=>{
+  username.value=details
+  userDetails()
+})
+
 </script>
 
 <template>
-  <Header />
+<div class="head">
+  <nav style="background-color: #d538fc;" class="navbar sticky-top navbar-expand-lg">
+    <div class="container-fluid m-auto">
+      <router-link class="navbar-brand text-white" to="/">Connect Me</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item text-primary">
+            <router-link class="nav-link active  text-white text-uppercase" to="/">Home</router-link>
+          </li>
+          <div class="d-flex" v-if="username">
+            <div v-if="details.role == 'job_seeker'" class="">
+              <li class="nav-item text-uppercase">
+                <router-link class="nav-link text-white" to="/J/dashboard">Dashboard</router-link>
+              </li>
+            </div>
+            <div v-else-if="details.role == 'employer'" class="">
+              <li class="nav-item text-uppercase">
+                <router-link class="nav-link text-white" to="/e/dashboard">Dashboard</router-link>
+              </li>
+            </div>
+            <div v-else class="">
+              <li class="nav-item text-uppercase">
+                <router-link class="nav-link text-white" to="/admin/dashboard">Dashboard</router-link>
+              </li>
+            </div>
+
+            <li class="nav-item text-uppercase">
+              <img width="50" @click="showProfile = showProfile === false ? true : false" style="border-radius: 50%;" height="50"  :src="'http://127.0.0.1:8000/Profiles/'+details.picture"  alt="">
+            </li>
+
+          </div>
+          <div class="d-flex" v-else>
+            <li class="nav-item d-flex justify-content-center align-items-center text-uppercase">
+              <router-link to="/auth/login" class="text-decoration-none">LOGIN <i style="font-size: 22px" @click="LogOut()" class="bi bi-box-arrow-right"></i></router-link>
+            </li>
+            <li class="nav-item d-flex justify-content-center align-items-center text-uppercase">
+              <router-link to="/auth/register" class="text-decoration-none">Get Started <i style="font-size: 22px" class="bi bi-pen"></i></router-link>
+            </li>
+          </div>
+
+
+
+          <div  :class="showProfile ? '':'d-none'"    class="showProfile d-flex justify-content-center align-items-center m-2 border p-4">
+            <div class="profile" v-if="username">
+              <div class="d-flex justify-content-center">
+                <img style="border-radius: 50%;" width="50"  height="50"  :src="'http://127.0.0.1:8000/Profiles/'+details.picture"   alt="">
+              </div>
+              <p class="text-center text-uppercase"> {{details.firstName}} {{details.lastName}}</p>
+              <div class="d-flex btn border">
+                <button style="background-color: yellow;" class="button btn btn-sm">Active</button>
+                <button class="button btn btn-sm btn-light">In active</button>
+              </div>
+              <router-link to="/user/profile" style="font-size: 23px;" class="text-decoration-none"><i style="font-size: 22px" class="bi bi-gear"> </i>Settings</router-link>
+
+              <br>
+              <i style="font-size: 22px" @click="LogOut()" class="bi bi-box-arrow-left">  Logout</i>
+            </div>
+          </div>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+</div>
   <div class="main container">
       <div class="row intro my-2 py-2">
       <div class="col col-md-6 col-lg-6">
@@ -47,6 +133,24 @@ import Footer from "@/views/Footer.vue";
 
 </template>
 <style>
+.showProfile{
+  border-radius: 8px;
+  background-color: ghostwhite;
+  position: absolute;
+  right: 2rem;
+  top: 2rem;
+  height: 18rem;
+  width: 14rem;
+}
+.profile{
+  height: 13rem;
+  width: 10rem
+}
+li:hover{
+  background: rgb(146, 159, 222);
+  color: white;
+  cursor: progress;
+}
 .intro{
   justify-content: right ;
   align-items: center;
