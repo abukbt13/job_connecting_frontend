7d457f74a484fb4b8ebf686e4d8cf22019b77e28
Freemano <freemano@Freeman.local>
2024-04-10 03:49:18 +0300

    auth changes

--- src/Composables/auth.js
+++ src/Composables/auth.js
@@ -7,7 +7,7 @@ export function auth(){
     const base_url = ref('https://jobconnecting.kwetunyumbani.online/api/')
     const storage = ref('https://jobconnecting.kwetunyumbani.online/backend/public/')
 
-    // const base_url = ref('http://127.0.0.1:8000/api/')
+    // const base_url = ref('http://127.0.0.1:8000/api/')git
     // const storage = ref('http://127.0.0.1:8000/')
 
     const   authUser = async () => {
